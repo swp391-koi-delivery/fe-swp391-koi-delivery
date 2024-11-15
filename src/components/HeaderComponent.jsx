c5a7bf88e9ffbb5b7f213ecddaaf3b79d70ac49f
thephong183785 <phongdtse183785@fpt.edu.vn>
2024-11-16 00:08:33 +0700

    update 15/11/2024

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -309,51 +309,38 @@ function HeaderComponent() {
                           <path d="M7.99999 14.9C7.84999 14.9 7.72499 14.85 7.59999 14.75L1.84999 9.10005C1.62499 8.87505 1.62499 8.52505 1.84999 8.30005C2.07499 8.07505 2.42499 8.07505 2.64999 8.30005L7.99999 13.525L13.35 8.25005C13.575 8.02505 13.925 8.02505 14.15 8.25005C14.375 8.47505 14.375 8.82505 14.15 9.05005L8.39999 14.7C8.27499 14.825 8.14999 14.9 7.99999 14.9Z" />
                         </svg>
                       </a>
-                      {user?.role === "CUSTOMER" ? (
-                        <div className="submenu relative left-0 top-full hidden w-[250px] rounded-sm bg-white p-4 transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark-2 lg:invisible lg:absolute lg:top-[110%] lg:block lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full">
-                          <Link
-                            to="/"
-                            className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
-                          >
-                            Home Page
-                          </Link>
-                          <Link
-                            to="/order"
-                            className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
-                          >
-                            Order Page
-                          </Link>
-                          <Link
-                            to="/order-list"
-                            className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
-                          >
-                            Order List Page
-                          </Link>
-                          <Link
-                            to="/order-history"
-                            className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
-                          >
-                            Order History Page
-                          </Link>
-                          <Link
-                            to="/order-search"
-                            className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
-                          >
-                            Order Search Page
-                          </Link>
-                        </div>
-                      ) : (
-                        user == null && (
-                          <div className="submenu relative left-0 top-full hidden w-[250px] rounded-sm bg-white p-4 transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark-2 lg:invisible lg:absolute lg:top-[110%] lg:block lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full">
-                            <Link
-                              to="/order-search"
-                              className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
-                            >
-                              Order Search Page
-                            </Link>
-                          </div>
-                        )
-                      )}
+                      <div className="submenu relative left-0 top-full hidden w-[250px] rounded-sm bg-white p-4 transition-[top] duration-300 group-hover:opacity-100 dark:bg-dark-2 lg:invisible lg:absolute lg:top-[110%] lg:block lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full">
+                        <Link
+                          to="/"
+                          className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+                        >
+                          Home Page
+                        </Link>
+                        <Link
+                          to="/order"
+                          className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+                        >
+                          Order Page
+                        </Link>
+                        <Link
+                          to="/order-list"
+                          className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+                        >
+                          Order List Page
+                        </Link>
+                        <Link
+                          to="/order-history"
+                          className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+                        >
+                          Order History Page
+                        </Link>
+                        <Link
+                          to="/order-search"
+                          className="block rounded px-4 py-[10px] text-sm text-body-color hover:text-primary dark:text-dark-6 dark:hover:text-primary"
+                        >
+                          Order Search Page
+                        </Link>
+                      </div>
                     </li>
                   </ul>
                 </nav>
